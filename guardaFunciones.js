84a8211dd0a1348aba7b215f6b9cccc7ab9a46c7
Joshua <josh93delosangeles@gmail.com>
2022-05-09 16:48:44 +0200

    cambio

---
+++ guardaFunciones.js
@@ -0,0 +1,31 @@
+function suma (x,y)
+{
+let resultado = x+y
+console.log(resultado)
+return resultado
+};
+
+
+
+function resta (x,y)
+{
+let resultado = x-y
+console.log(resultado)
+return resultado
+
+};
+ 
+
+function multi (x,y)
+{
+let resultado = x*y
+console.log(resultado)
+return resultado 
+};
+function div (x,y)
+{
+    let resultado = x/y
+    console.log(resultado)
+    return resultado
+
+};
